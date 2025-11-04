7c1b7ad3300703b90ab522064f80109e426b194e
MortyUganda <kurganskiy.1990@mail.ru>
2025-11-04 18:04:16 +0300

    rename test

--- tests/test_dashboard.py
+++ tests/test_dashboard.py
@@ -5,7 +5,7 @@ from pages.dashboard_page import DashboardPage
 
 @pytest.mark.dashboard
 @pytest.mark.regression
-def test_dashboard_component(dashboard_page_with_state: DashboardPage):
+def test_dashboard_displaying(dashboard_page_with_state: DashboardPage):
     dashboard_page_with_state.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/dashboard')
     dashboard_page_with_state.check_visible_dashboard_title()
     dashboard_page_with_state.check_visible_students_chart()
