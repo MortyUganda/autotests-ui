9036751bb6410a082d42a70e00b8c1e74e92e02b
MortyUganda <kurganskiy.1990@mail.ru>
2025-11-04 17:40:58 +0300

    7.3 Знакомство с PageComponent

---
+++ navigation/sidebar_component.py
@@ -0,0 +1,27 @@
+import re
+
+from playwright.sync_api import expect, Page
+from components.base_component import BaseComponent
+from navigation.sidebar_list_item_component import SidebarListItemComponent
+
+class SidebarComponent(BaseComponent):
+    def __init__(self, page: Page) -> None:
+        super().__init__(page)
+
+        self.logout_list_item = SidebarListItemComponent(page, 'logout')
+        self.dashboard_list_item = SidebarListItemComponent(page, 'dashboard')
+        self.courses_list_item = SidebarListItemComponent(page, 'courses')
+
+    def check_visible(self):
+        self.logout_list_item.check_visible('Dashboard')
+        self.dashboard_list_item.check_visible('Courses')
+        self.logout_list_item.check_visible('Logout')
+
+    def click_logout(self):
+        self.logout_list_item.navigate(re.compile(r'.*/#/auth/login'))
+
+    def click_courses(self):
+        self.courses_list_item.navigate(re.compile(r'.*/#/courses'))
+
+    def click_dashboard(self):
+        self.dashboard_list_item.navigate(re.compile(r'.*/#/dashboard'))
