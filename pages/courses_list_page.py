769a735cd6d890157d7f4fb84fc2a355d281148d
MortyUganda <kurganskiy.1990@mail.ru>
2025-11-07 23:17:22 +0300

    7.4 Практикуемся в реализации PageComponent

--- pages/courses_list_page.py
+++ pages/courses_list_page.py
@@ -6,37 +6,21 @@ from components.views.empty_view_component import EmptyViewComponent
 from navigation.navbar_component import NavbarComponent
 from navigation.sidebar_component import SidebarComponent
 from pages.base_page import BasePage
-
+from components.courses.courses_list_toolbar_view_component import CoursesListToolbarViewComponent
 
 class CoursesListPage(BasePage):
     def __init__(self, page: Page):
         super().__init__(page)
 
-        self.courses_title = page.get_by_test_id('courses-list-toolbar-title-text')
-        self.create_course_button = page.get_by_test_id('courses-list-toolbar-create-course-button')
-
+        self.toolbar_view = CoursesListToolbarViewComponent(page)
         self.navbar = NavbarComponent(page)
         self.sidebar = SidebarComponent(page)
         self.empty_view = EmptyViewComponent(page, 'courses-list')
         self.course_view = CourseViewComponent(page)
         self.view_menu_component = CourseViewMenuComponent(page)
 
-        self.course_menu_button = page.get_by_test_id('course-view-menu-button')
-        self.course_edit_menu_item = page.get_by_test_id('course-view-edit-menu-item')
-        self.course_delete_menu_item = page.get_by_test_id('course-view-delete-menu-item')
-
-    def check_visible_courses_title(self):
-        expect(self.courses_title).to_be_visible()
-        expect(self.courses_title).to_have_text('Courses')
-
     def check_visible_empty_view(self):
         self.empty_view.check_visible(
             title='There is no results',
             description='Results from the load test pipeline will be displayed here'
         )
-
-    def check_visible_create_course_button(self):
-        expect(self.create_course_button).to_be_visible()
-
-    def click_create_course_button(self):
-        self.create_course_button.click()
