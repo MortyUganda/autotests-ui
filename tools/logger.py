4318cdf0927a6546edf76d0914e999e65cf42dd0
MortyUganda <kurganskiy.1990@mail.ru>
2025-11-19 22:13:36 +0300

    9.5 Логирование взаимодействий со страницей. Практика работы с логированием

--- tools/logger.py
+++ tools/logger.py
@@ -8,7 +8,7 @@ def get_logger(name: str) -> logging.Logger:
     handler = logging.StreamHandler()
     handler.setLevel(logging.DEBUG)
 
-    formatter = logging.Formatter('%(asctime)s | %(name)s | %(levelname)s | %(message)s')
+    formatter = logging.Formatter('%(asctime)s | %(name)s | %(levelname)s | %(message)s') # %(filename)s - имя файла, в котором записано сообщение
     handler.setFormatter(formatter)  # Применяем форматтер к обработчику
 
     # Добавляем обработчик к логгеру
