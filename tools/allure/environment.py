4e604cec78af7428f1b27db5e6615d0ae9fb1e79
MortyUganda <kurganskiy.1990@mail.ru>
2025-11-21 21:23:25 +0300

    10.4 Запуск автотестов на CI

--- tools/allure/environment.py
+++ tools/allure/environment.py
@@ -7,7 +7,7 @@ logger = get_logger('create_allure_environment_file')
 
 def create_allure_environment_file():
     items = [f'{key}={value}' for key, value in settings.model_dump().items()]
-    properties = '\n'.join(items)
+    properties = '\n'.join(items )
 
     with open(settings.allure_results_dir.joinpath('environment.properties'), 'w+') as file:
         file.write(properties)  # Записываем переменные в файл
