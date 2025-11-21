4659b759f3f129ed22c991afa7aca92b3767ece2
MortyUganda <kurganskiy.1990@mail.ru>
2025-11-21 20:22:14 +0300

    Добавили лог в environment.py

--- tools/allure/environment.py
+++ tools/allure/environment.py
@@ -1,7 +1,9 @@
 import platform
 import sys
 from config import settings
+from tools.logger import get_logger
 
+logger = get_logger('create_allure_environment_file')
 
 def create_allure_environment_file():
     items = [f'{key}={value}' for key, value in settings.model_dump().items()]
@@ -11,3 +13,4 @@ def create_allure_environment_file():
         file.write(properties)  # Записываем переменные в файл
         file.write(f'\nos_info = {platform.system()} = {platform.release()}')
         file.write(f'\npython_version = {sys.version}')
+        logger.info(f'Создали файл environment.properties')
